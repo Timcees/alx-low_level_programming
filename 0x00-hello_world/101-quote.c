2b44cb22859886502f83a279960ad8d93e9587aa
Jaygigs <omowaletimothy@gmail.com>
2022-09-09 03:25:03 -0700

    simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful" -Dora kopar, 2015-10-19", followed by a new line, to standard error.
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	write(2,"and that  piece of art is useful\n" -Dora Korpar, 2015-10-19\n",59);
+	return (o);
+}
